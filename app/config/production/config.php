34e38a864f7f797b9f5e09e5377cd8e58ba122d4
Abdul Manan <abdulmanan7@hotmail.com>
2015-08-22 10:34:44 +0500

    production config base change

--- app/config/production/config.php
+++ app/config/production/config.php
@@ -1,14 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-function __autoload($classname) {
-	if (strpos($classname, 'CI_') !== 0) {
-		$file = APPPATH . 'libraries/' . $classname . '.php';
-		if (file_exists($file) && is_file($file)) {
-			@include_once $file;
-		}
-	}
-}
 /*
 |--------------------------------------------------------------------------
 | Base Site URL
@@ -25,7 +17,7 @@ function __autoload($classname) {
 | environments.
 |
  */
-$config['base_url'] = 'http://sajidshah.com/stms';
+$config['base_url'] = 'http://localhost/projects/stms/';
 
 /*
 |--------------------------------------------------------------------------
