5d9ddd118a8fad8c28ece4a9ec513f5890fcbaf8
Abdul Manan <abdulmanan7@hotmail.com>
2015-08-31 12:41:32 +0500

    dev branch added

--- app/config/production/config.php
+++ app/config/production/config.php
@@ -29,7 +29,7 @@ $config['base_url'] = 'http://sajidshah.com/stms/';
 | variable so that it is blank.
 |
  */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
